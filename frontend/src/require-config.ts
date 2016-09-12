c419e27520e6e4f82e4ebbff5674db2d74a752be
Molsbee <molsbee@gmail.com>
2016-09-12 10:31:28 -0500

    Added bootstrap.js to require config also stubbed out navbar and side bar menu.

--- frontend/src/require-config.ts
+++ frontend/src/require-config.ts
@@ -2,6 +2,7 @@ declare var require: any;
 require.config({
     paths: {
         "knockout": "externals/knockout-3.3.0",
-        "jquery": "externals/jquery-2.1.4.min"
+        "jquery": "externals/jquery-2.1.4.min",
+        "bootstrap": "external/bootstrap.min"
     }
 });
